d7d53429a87e087a96cdd85b24e0063a118d2b97
Matthias Leuffen <matthes@leuffen.de>
2023-10-11 12:17:08 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -26,3 +26,4 @@ import "./slideshow/slideshow";
 import "./details-title/details-title";
 import "./typewriter-element/typewriter-element";
 import "./slider/liscom-slider";
+export * from "./loader/loader";
